6ea4cc8ac7448cdcf88f1148f74017caeb217e2c
Eduardo Carvalho Valadão <eduardovaladao2006@gmail.com>
2024-11-07 12:28:56 -0300

    App em progressão

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-import 
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+export default function App({ navigation }) {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Início" component="" />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
